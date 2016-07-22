d17c524d78e309049ea9c32a35d804f8dbede12d
河相　尚登 <h_kawai@plsize.co.jp>
2016-07-22 10:09:19 +0900

    entity

---
+++ src/main/java/library/entity/Search.java
@@ -0,0 +1,65 @@
+package library.entity;
+
+public class Search {
+	private int bookId;
+	private int isbn;
+	private String bookName;
+	private String authorName;
+	private int shelfId;
+	private String documentName;
+	private String statusName;
+	private String libraryName;
+
+	public int getBookId() {
+		return bookId;
+	}
+	public void setBookId(int bookId) {
+		this.bookId = bookId;
+	}
+
+	public String getBookName() {
+		return bookName;
+	}
+	public void setBookName(String bookName) {
+		this.bookName = bookName;
+	}
+	public String getAuthorName() {
+		return authorName;
+	}
+	public void setAuthorName(String authorName) {
+		this.authorName = authorName;
+	}
+
+
+	public int getIsbn() {
+		return isbn;
+	}
+	public void setIsbn(int isbn) {
+		this.isbn = isbn;
+	}
+	public int getShelfId() {
+		return shelfId;
+	}
+	public void setShelfId(int shelfId) {
+		this.shelfId = shelfId;
+	}
+	public String getDocumentName() {
+		return documentName;
+	}
+	public void setDocumentName(String documentName) {
+		this.documentName = documentName;
+	}
+	public String getStatusName() {
+		return statusName;
+	}
+	public void setStatusName(String statusName) {
+		this.statusName = statusName;
+	}
+	public String getLibraryName() {
+		return libraryName;
+	}
+	public void setLibraryName(String libraryName) {
+		this.libraryName = libraryName;
+	}
+
+}

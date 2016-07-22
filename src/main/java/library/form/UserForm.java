16d66b61c149bacf62613e881c6cce4f0a308de4
河相　尚登 <h_kawai@plsize.co.jp>
2016-07-22 10:06:09 +0900

    UserForm

---
+++ src/main/java/library/form/UserForm.java
@@ -0,0 +1,47 @@
+package library.form;
+
+public class UserForm {
+
+	private String userId;
+	private String userName;
+	private String address;
+	private String mail;
+	private String phoneNumber;
+	private int libraryId;
+	public String getUserId() {
+		return userId;
+	}
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getMail() {
+		return mail;
+	}
+	public void setMail(String mail) {
+		this.mail = mail;
+	}
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+	public int getLibraryId() {
+		return libraryId;
+	}
+	public void setLibraryId(int libraryId) {
+		this.libraryId = libraryId;
+	}
+}

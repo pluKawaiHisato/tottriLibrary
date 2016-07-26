9d7c1f55c772256f25f28ed4a0a11f85e90217f1
河相　尚登 <h_kawai@plsize.co.jp>
2016-07-26 14:29:36 +0900

    Reserve

---
+++ src/main/java/library/entity/Reserve.java
@@ -0,0 +1,41 @@
+package library.entity;
+
+public class Reserve {
+	private String userId;
+	private String isbn;
+	private int count;
+	private int libraryId;
+	private int bookId;
+
+	public String getUserId() {
+		return userId;
+	}
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+	public String getIsbn() {
+		return isbn;
+	}
+	public void setIsbn(String isbn) {
+		this.isbn = isbn;
+	}
+	public int getCount() {
+		return count;
+	}
+	public void setCount(int count) {
+		this.count = count;
+	}
+	public int getLibraryId() {
+		return libraryId;
+	}
+	public void setLibraryId(int libraryId) {
+		this.libraryId = libraryId;
+	}
+	public int getBookId() {
+		return bookId;
+	}
+	public void setBookId(int bookId) {
+		this.bookId = bookId;
+	}
+
+}

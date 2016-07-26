ce7e4778a6a8b86b8541748164c57665211089b6
河相　尚登 <h_kawai@plsize.co.jp>
2016-07-26 14:30:06 +0900

    ReserveForm

---
+++ src/main/java/library/form/ReserveForm.java
@@ -0,0 +1,18 @@
+package library.form;
+
+public class ReserveForm {
+	private String userId;
+
+	public String getUserId() {
+		return userId;
+	}
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+
+
+
+
+
+
+}

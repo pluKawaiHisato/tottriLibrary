e481c9cdc630d2156e568cbb0c08dc183b445d8f
河相　尚登 <h_kawai@plsize.co.jp>
2016-08-01 13:36:46 +0900

    8/1 check (entity)

---
+++ src/main/java/library/entity/Check.java
@@ -0,0 +1,83 @@
+package library.entity;
+
+public class Check {
+	private int bookId;
+	private String isbn;
+	private String bookName;
+	private String authorName;
+	private String publisher;
+	private String shelfId;
+	private String documentName;
+	private String statusName;
+	private int    libraryId;
+	private String libraryName;
+	private String documentId;
+
+	public String getDocumentId() {
+		return documentId;
+	}
+	public void setDocumentId(String documentId) {
+		this.documentId = documentId;
+	}
+	public int getBookId() {
+		return bookId;
+	}
+	public void setBookId(int bookId) {
+		this.bookId = bookId;
+	}
+	public String getIsbn() {
+		return isbn;
+	}
+	public void setIsbn(String isbn) {
+		this.isbn = isbn;
+	}
+	public String getBookName() {
+		return bookName;
+	}
+	public void setBookName(String bookName) {
+		this.bookName = bookName;
+	}
+	public String getAuthorName() {
+		return authorName;
+	}
+	public void setAuthorName(String authorName) {
+		this.authorName = authorName;
+	}
+	public String getPublisher() {
+		return publisher;
+	}
+	public void setPublisher(String publisher) {
+		this.publisher = publisher;
+	}
+	public String getShelfId() {
+		return shelfId;
+	}
+	public void setShelfId(String shelfId) {
+		this.shelfId = shelfId;
+	}
+	public String getDocumentName() {
+		return documentName;
+	}
+	public void setDocumentName(String documentName) {
+		this.documentName = documentName;
+	}
+	public String getStatusName() {
+		return statusName;
+	}
+	public void setStatusName(String statusName) {
+		this.statusName = statusName;
+	}
+	public int getLibraryId() {
+		return libraryId;
+	}
+	public void setLibraryId(int libraryId) {
+		this.libraryId = libraryId;
+	}
+	public String getLibraryName() {
+		return libraryName;
+	}
+	public void setLibraryName(String libraryName) {
+		this.libraryName = libraryName;
+	}
+
+}
